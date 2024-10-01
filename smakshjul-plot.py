4dcd75cbdb9a1728f8dbab45bdac332d00def8d0
Espen Breivik <espen.breivik@hey.com>
2024-10-02 01:25:03 +0200

    Much farther - something is wrong with color computing tho - need to re-evaluate unique-counting

--- smakshjul-plot.py
+++ smakshjul-plot.py
@@ -19,8 +19,29 @@ def read_data(fname):
 
     return data
 
+def fetch_category_data(vals, data_idx: int, color_idx: int):
+    # didn't find another easy way to re-combine arrays
+    cols = np.column_stack((vals[:,data_idx], vals[:,color_idx]))
+    # fetch colors
+    idx = np.unique(cols[:,1], return_index=True)[1]
+    # sort them since they were sorted on the way out
+    #idx = idx.argsort()[::-1]
+    idx.sort()
+    # refetch the indexes in the correct order and convert to int
+    o_colors = cols[idx][:,1].astype(int)[::-1]
 
-data = read_data('smakshjul-data.csv')
+    # fetch category data with counts
+    idx, counts = np.unique(cols[:,0], return_index=True, return_counts=True)[1:]
+    # construct a new array combined with counts to be able to sort them
+    sub_tot = np.column_stack((idx, counts))
+    # mind-blowing way to return sorted indixes based on first column
+    # also note that this is inverted since the data direction is different from the labeling direction
+    o_data = sub_tot[sub_tot[:,0].argsort()[::-1]] 
+
+    return (o_colors, o_data[:,1])
+
+
+data = read_data('smakshjul-data-small.csv')
 
 fig, ax = plt.subplots(figsize=(13,12))
 
@@ -31,23 +52,15 @@ mid_radius = outer_radius - outer_size
 inner_radius = mid_radius - mid_size
 text_margin = 0.02
 
-vals = np.array([[60., 32.], [37., 40.], [29., 10.]])
+vals = np.array(data)
 
-cmap = plt.colormaps["tab20c"]
-outer_colors = cmap([1, 2, 5, 6, 9, 10])
-mid_colors = cmap(np.arange(3)*4)
-inner_colors = cmap([2, 6, 10])
+o_colors, o_data = fetch_category_data(vals, 2, 4)
+i_colors, i_data = fetch_category_data(vals, 1, 4)
 
-ax.pie(vals.sum(axis=1), radius=outer_radius, colors=outer_colors,
-       wedgeprops=dict(width=outer_size, edgecolor='w'))
-
-ax.pie(vals.flatten(), radius=mid_radius, colors=mid_colors,
-       wedgeprops=dict(width=mid_size, edgecolor='w'))
-
-ax.pie(vals.flatten(), radius=inner_radius, colors=inner_colors,
-       wedgeprops=dict(edgecolor='w'))
-
-ax.set(aspect="equal")
+cmap = plt.colormaps["tab20b"]
+outer_colors = cmap(o_colors)
+mid_colors = cmap(o_colors)
+inner_colors = cmap(i_colors)
 
 angle_step = 360.0 / len(data)
 
@@ -55,17 +68,58 @@ angle = 90
 angle_ofs = 0
 ha = 'left'
 
+start_angle = angle + angle_step / 2
+
+ax.pie(o_data, radius=outer_radius, colors=outer_colors,
+       startangle=start_angle,
+       wedgeprops=dict(width=outer_size, edgecolor='w'))
+
+ax.pie(o_data, radius=mid_radius, colors=mid_colors,
+       startangle=start_angle,
+       wedgeprops=dict(width=mid_size, edgecolor='w'))
+
+ax.pie(i_data, radius=inner_radius, colors=inner_colors,
+       startangle=start_angle,
+       wedgeprops=dict(edgecolor='w'))
+
+ax.set(aspect="equal")
 
 for line in data:
     r_a = math.radians(angle)
-    x = (mid_radius + text_margin) * math.cos(r_a)
-    y = (mid_radius + text_margin) * math.sin(r_a)
 
+    o_pos = (mid_radius + text_margin)
+    m_pos = (inner_radius + text_margin)
+    i_pos = (0.2 + text_margin)
+
+    x = o_pos * math.cos(r_a)
+    y = o_pos * math.sin(r_a)
+
+    # outer text
     tx = ax.text(x, y, str.upper(line[0]),
                             size=10, rotation=angle + angle_ofs,
                             horizontalalignment=ha, verticalalignment='center',
                             rotation_mode='anchor',
                             transform=ax.transData)
+    
+    x = m_pos * math.cos(r_a)
+    y = m_pos * math.sin(r_a)
+
+    tx = ax.text(x, y, str.upper(line[2]),
+                            size=10, rotation=angle + angle_ofs,
+                            horizontalalignment=ha, verticalalignment='center',
+                            rotation_mode='anchor',
+                            transform=ax.transData)
+    
+
+    x = i_pos * math.cos(r_a)
+    y = i_pos * math.sin(r_a)
+
+    # tx = ax.text(x, y, str.upper(line[1]),
+    #                         size=10, rotation=angle + angle_ofs,
+    #                         horizontalalignment=ha, verticalalignment='center',
+    #                         rotation_mode='anchor',
+    #                         transform=ax.transData)
+
     angle -= angle_step
 
     if angle < -90:
