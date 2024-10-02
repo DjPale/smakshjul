de263699de4d3e4c4a729c12089cdeb8592f5213
Espen Breivik <espen.breivik@hey.com>
2024-10-02 22:17:04 +0200

    * Colors fixed now! * Category labels kinda work * Checkin before refactor label plotting to functions

--- smakshjul-plot.py
+++ smakshjul-plot.py
@@ -22,10 +22,9 @@ def read_data(fname):
 def fetch_category_data(vals, data_idx: int, color_idx: int):
     # didn't find another easy way to re-combine arrays
     cols = np.column_stack((vals[:,data_idx], vals[:,color_idx]))
-    # fetch colors
-    idx = np.unique(cols[:,1], return_index=True)[1]
-    # sort them since they were sorted on the way out
-    #idx = idx.argsort()[::-1]
+    # fetch unique categories - the index they "switch" to a new value will determine which color to use
+    idx = np.unique(cols[:,0], return_index=True)[1]
+    # sort them since they were sorted int the unique-function
     idx.sort()
     # refetch the indexes in the correct order and convert to int
     o_colors = cols[idx][:,1].astype(int)[::-1]
@@ -35,13 +34,13 @@ def fetch_category_data(vals, data_idx: int, color_idx: int):
     # construct a new array combined with counts to be able to sort them
     sub_tot = np.column_stack((idx, counts))
     # mind-blowing way to return sorted indixes based on first column
-    # also note that this is inverted since the data direction is different from the labeling direction
+    # also note that this is inverted since the data direction is different from the text print direction
     o_data = sub_tot[sub_tot[:,0].argsort()[::-1]] 
 
     return (o_colors, o_data[:,1])
 
 
-data = read_data('smakshjul-data-small.csv')
+data = read_data('smakshjul-data.csv')
 
 fig, ax = plt.subplots(figsize=(13,12))
 
@@ -84,6 +83,10 @@ ax.pie(i_data, radius=inner_radius, colors=inner_colors,
 
 ax.set(aspect="equal")
 
+
+cat_start_angle = 90
+cat_text = ''
+
 for line in data:
     r_a = math.radians(angle)
 
@@ -101,15 +104,23 @@ for line in data:
                             rotation_mode='anchor',
                             transform=ax.transData)
     
-    x = m_pos * math.cos(r_a)
-    y = m_pos * math.sin(r_a)
+    cur_cat = str.upper(line[2])
+    if cat_text == '':
+        cat_text = cur_cat
+    elif cat_text != cur_cat:
+        temp_angle = cat_start_angle - ((cat_start_angle - angle) / 2) + 2
+        r_a = math.radians(temp_angle)
 
-    tx = ax.text(x, y, str.upper(line[2]),
-                            size=10, rotation=angle + angle_ofs,
-                            horizontalalignment=ha, verticalalignment='center',
-                            rotation_mode='anchor',
-                            transform=ax.transData)
-    
+        x = m_pos * math.cos(r_a)
+        y = m_pos * math.sin(r_a)
+
+        tx = ax.text(x, y, cat_text,
+                                size=10, rotation=temp_angle + angle_ofs,
+                                horizontalalignment=ha, verticalalignment='center',
+                                rotation_mode='anchor',
+                                transform=ax.transData)
+        cat_text = cur_cat
+        cat_start_angle = angle
 
     x = i_pos * math.cos(r_a)
     y = i_pos * math.sin(r_a)
